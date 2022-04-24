02eaff67efc9512a565a8d21d2bcb541b8447ee1
MasterXiong <harryfootball@163.com>
2022-04-24 04:31:09 +0000

    add docker support

---
+++ run_docker_cpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Launch an experiment using the docker cpu image
+
+docker run --ipc=host --rm -it 
+  -v `pwd`:/home/$USER/volleyball/ 
+  stablebaselines/rl-baselines-zoo-cpu:v2.10.0 
+  /bin/bash
+
